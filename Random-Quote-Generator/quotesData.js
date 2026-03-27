1c5b098852da9a058102761eb9f4e4488d12b66a
Qasim <qjalal3636@gmail.com>
2026-03-27 10:54:25 +0500

    Save the data

---
+++ Random-Quote-Generator/quotesData.js
@@ -0,0 +1,7 @@
+const quotes = [
+    { text: "Life is short, make it count.", author: "Anonymous" },
+    { text: "Code is like poetry.", author: "Developer" },
+    { text: "Stay hungry, stay foolish.", author: "Steve Jobs" },
+    { text: "Knowledge is power.", author: "Francis Bacon" },
+    { text: "Simplicity is the ultimate sophistication.", author: "Leonardo da Vinci" }
+];
