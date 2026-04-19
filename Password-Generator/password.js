ddbb42290ebad562c84a04d5ef9f492c65d44f56
Qasim <qjalal3636@gmail.com>
2026-04-19 13:44:18 +0500

    use the query selector to access the btn,inputs and checkboxes

---
+++ Password-Generator/password.js
@@ -0,0 +1,10 @@
+const generateBtn = document.querySelector('.js-generate-button');
+const passwordInput = document.querySelector('.js-password-input');
+const passwordLength = document.querySelector('.password-length-input');
+const upperBox = document.querySelector('#upper-box');
+const lowerBox = document.querySelector('#lower-box');
+const numbersBox = document.querySelector('#numbers-box');
+const symbolsBox = document.querySelector('#symbols-box');
+
+
+ 
