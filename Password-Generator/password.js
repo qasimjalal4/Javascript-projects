e3e71e3a2c80bd4adfb27d4a8dc7dedadf27f7db
Qasim <qjalal3636@gmail.com>
2026-04-19 14:24:27 +0500

    add the logic to generate password

--- Password-Generator/password.js
+++ Password-Generator/password.js
@@ -7,4 +7,50 @@ const numbersBox = document.querySelector('#numbers-box');
 const symbolsBox = document.querySelector('#symbols-box');
 
 
+const UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const LOWER = 'abcdefghijklmnopqrstuvwxyz';
+const NUMBERS = '0123456789';
+const SYMBOLS = '!@#$%^&*()_+{}[]<>?'; 
+
+
+generateBtn.addEventListener('click', () => {
+
+  let container = '';
+  let password = '';
+
+  if(upperBox.checked) {
+    container += UPPER;
+  }
+
+  if(lowerBox.checked) {
+    container += LOWER;
+  }
+  
+  if(numbersBox.checked) {
+    container += NUMBERS;
+  }
+  
+  if(symbolsBox.checked) {
+    container += SYMBOLS;
+  }
+
+  if(container === '') {
+    alert('Please atleast select one box!');
+    return;
+  }
+
+  
+  let length = Number(passwordLength.value);
+
+ while(password.length !== length) {
+
+  let index =  Math.floor(Math.random() * container.length);
+
+  password += container[index];
+ }
+
+ passwordInput.value = password;
+  
  
+  
+})
