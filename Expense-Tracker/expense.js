f02dbcaa4e79a8e9f3850470a4886dfa94b3a4ec
Qasim <qjalal3636@gmail.com>
2026-05-03 15:57:34 +0500

    created addExpense function to add object in expense array

--- Expense-Tracker/expense.js
+++ Expense-Tracker/expense.js
@@ -1,8 +1,49 @@
 const itemBox = document.querySelector('.item-box');
 const amountBox = document.querySelector('.amount-box');
-const dateInputEl = document.querySelector('.date-input');
+const dateInputEl = document.querySelector('.input-date');
 const expenseCategorySelector =  document.querySelector('.expense-category');
 const addBtn = document.querySelector('.add-button');
-const totalAmount = document.querySelector('.#total-amount')
+const totalAmount = document.querySelector('#total-amount')
 const totalQuantity = document.querySelector('#total-quantity');
 const highestExpense = document.querySelector('#highest-expense');
+
+
+let expenses = [
+  {
+    item: 'Lunch',
+    category: 'Food',
+    price: 250,
+    date: '2026-06-10'
+  }
+]
+
+
+function renderExpense() {
+  
+}
+
+
+function addExpense() {
+  let item = itemBox.value;
+  let category = expenseCategorySelector.value;
+  let price = Number(amountBox.value);
+  let date = dateInputEl.value;
+
+
+  expenseList.push({
+    item,
+    category,
+    price,
+    date
+  })
+
+
+  itemBox.value = '';
+  expenseCategorySelector.value = '';
+  amountBox.value = '';
+  dateInputEl.value = '';
+}
+
+addBtn.addEventListener('click', () => {
+     addExpense();
+})
