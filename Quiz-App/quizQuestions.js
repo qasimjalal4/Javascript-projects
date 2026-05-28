75231e194a739f11c32980fa546db8822bf02806
Qasim <qjalal3636@gmail.com>
2026-05-28 12:39:28 +0500

    link the css and js files with html file and stores questions data in quizQuestions.js

---
+++ Quiz-App/quizQuestions.js
@@ -0,0 +1,46 @@
+const quizQuestions = [{
+  question: 'What is the difference between let and const?',
+  option: [
+    'Both can be reassigned',
+    'Both cannot be reassigned',
+    'let can be reassigned, const cannot',
+    'const can be reassigned, let cannot'
+  ],
+  correctAnswerIndex: 2
+}, {
+  question: 'What does HTML stand for?',
+  option: [
+    'HyperText Markup Language',
+    'High Text MAchine Language',
+    'Hyperlinks Text Mark Language',
+    'None of these'
+  ],
+  correctAnswerIndex: 0
+},{
+  question: 'Which company developed JavaScript?',
+  option: [
+     'Microsoft',
+     'Netscape',
+     'Google',
+     'Apple' 
+  ],
+  correctAnswerIndex: 1
+}, {
+  question: 'Which language is used to style web pages?',
+  option: [
+    'HTML',
+    'JQuery',
+    'CSS',
+    'Python'
+  ],
+  correctAnswerIndex: 2
+}, {
+  question: 'Which symbol is used for comments in JavaScript?',
+  option: [
+    '//',
+    '/* */',
+    '#',
+    '$'
+  ],
+  correctAnswerIndex: 0
+}];
